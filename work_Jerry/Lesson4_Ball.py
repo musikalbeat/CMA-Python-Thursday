e821188bd8317900f17bf7d5b7ef1af05a263a5b
Jerry5311 <84550672+Jerry5311@users.noreply.github.com>
2021-06-10 16:15:45 -0700

    Update Lesson4_Ball.py

--- work_Jerry/Lesson4_Ball.py
+++ work_Jerry/Lesson4_Ball.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Movement in PyGames")
 
 class Ball:
+
     def __init__(self, x, y, radius, color, screen):
         self.x = x
         self.y = y
@@ -17,6 +18,7 @@ class Ball:
         self.screen = screen
         self.x_speed = 0
         self.y_speed = 0
+        self.speed = 3
 
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
@@ -28,18 +30,29 @@ class Ball:
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    self.y_speed -= 7
+                    self.y_speed -= self.speed
                 elif event.type == pygame.K_DOWN:
-                     self.y_speed += 7
+                     self.y_speed += self.speed
                 elif event.type == pygame.K_LEFT:
-                     self.y_speed -= 7
+                     self.y_speed -= self.speed
                 elif event.type == pygame.K_RIGHT:
-                     self.y_speed += 7
+                     self.y_speed += self.speed
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.y_speed += self.speed
+                elif event.type == pygame.K_DOWN:
+                     self.y_speed -= self.speed
+                elif event.type == pygame.K_LEFT:
+                     self.y_speed += self.speed
+                elif event.type == pygame.K_RIGHT:
+                     self.y_speed -= self.speed
+
+
         self.x += self.x_speed
         self.y += self.y_speed
 
 turquoise = (3, 252, 232)
-ball = Ball(225, 225, 20, turqouise, screen)
+ball = Ball(225, 225, 20, turquoise, screen)
 
 while True:
     screen.fill((0, 0, 0))
